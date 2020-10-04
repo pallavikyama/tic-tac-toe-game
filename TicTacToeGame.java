1b9fc6eb1071cd4500f2c9e68cd5883192c46308
pallavikyama <kyamap98@gmail.com>
2020-10-04 09:49:36 +0530

    Added:Player chooses one among X and O and the computer gets the other

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,7 +1,13 @@
 package com.blz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	static char[] board = new char[10];
+	static char playerLetter;
+	static char computerLetter;
+
+	static Scanner input = new Scanner(System.in);
 
 	// CREATE EMPTY TIC_TAC_TOE BOARD
 	private void createBoard() {
@@ -11,9 +17,28 @@ public class TicTacToeGame {
 		System.out.println("An empty board is created.");
 	}
 
+	// CHOOSING INPUT
+	private void gameInputs() {
+		System.out.println("Choose a letter to play the game: 'X'(capital_case X) or 'O'(capital_case O).");
+		boolean x = false;
+		while (x == false) {
+			playerLetter = input.next().charAt(0);
+			if (playerLetter == 'X' || playerLetter == 'O')
+				x = true;
+			else {
+				x = false;
+				System.out.println("Wrong input. Please try again. Enter 'X' or 'O' only.");
+			}
+		}
+		computerLetter = (playerLetter == 'X') ? 'O' : 'X';
+		System.out.println("Player has choosed a " + playerLetter + " and computer got a " + computerLetter + ".");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to TIC TAC TOE Game.");
 		TicTacToeGame boardObj = new TicTacToeGame();
 		boardObj.createBoard();
+		boardObj.gameInputs();
+		input.close();
 	}
 }
