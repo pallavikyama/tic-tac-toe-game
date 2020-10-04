f2fca6cd98d602b19fed453467e3933274a3d3f3
pallavikyama <kyamap98@gmail.com>
2020-10-04 09:43:21 +0530

    Added:Empty board - char array of size 10 is created

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,7 +1,19 @@
 package com.blz.tictactoe;
 
 public class TicTacToeGame {
+	static char[] board = new char[10];
+
+	// CREATE EMPTY TIC_TAC_TOE BOARD
+	private void createBoard() {
+		for (int i = 1; i < board.length; i++) {
+			board[i]=' ';
+		}
+		System.out.println("An empty board is created.");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to TIC TAC TOE Game.");
+		TicTacToeGame boardObj = new TicTacToeGame();
+		boardObj.createBoard();
 	}
 }
